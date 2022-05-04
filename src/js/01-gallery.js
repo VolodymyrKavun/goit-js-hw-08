e97ffab84058ed4dbac546a0540edb6ad6fc85d1
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-05-04 20:56:54 +0300

    add task 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,43 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import imageCardTpl from '../templates/image-card.hbs';
+
+import imagesCardsTpl from '../templates/images-cards.hbs';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+/**
+ *  Отримання доступу до елементів
+ */
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+/**
+ * Константа усіх елементів галереї
+ */
+const galleryMarkup = createCardsImageMarkup(galleryItems);
+
+/**
+ * Функція створення розмітки елементів галереї
+ */
+function createCardsImageMarkup(cards) {
+  return imagesCardsTpl(galleryItems);
+}
+
+/**
+ * Додавання розмітки в DOM
+ */
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+/**
+ * SimpleLightbox
+ */
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_CURRENT = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const dataSecond = data.seconds;
+  const secondLocalStorage = localStorage.setItem(TIME_CURRENT, dataSecond);
+};
+
+player.on('play', throttle(onPlay, 1000));
+
+const getLocalTime = localStorage.getItem(TIME_CURRENT);
+if (getLocalTime) {
+  player.setCurrentTime(Number(getLocalTime));
+}
