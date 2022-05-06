e024c9f4af2e902990f2f9ab05765256a15e19fc
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-05-06 18:01:46 +0300

    add 3 task and finished hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,34 @@
+// Підключення Ліб
 import vimeoPlayer from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const TIME_CURRENT = 'videoplayer-current-time';
+// Ключ локального сховища
+const TIME_CURRENT_KEY = 'videoplayer-current-time';
 
+// Доступ до елементів
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
+// Функція:
+// - відстеження події часу
+// - запис у локальне сховище
+// - перевірка
 const onPlay = function (data) {
   const dataSecond = data.seconds;
-  const secondLocalStorage = localStorage.setItem(TIME_CURRENT, dataSecond);
+  try {
+    localStorage.setItem(TIME_CURRENT_KEY, dataSecond);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-player.on('play', throttle(onPlay, 1000));
+// Відстеження часу відео
+player.on('timeupdate', throttle(onPlay, 1000));
 
-const getLocalTime = localStorage.getItem(TIME_CURRENT);
+// Отримання запису з локального сховища
+const getLocalTime = localStorage.getItem(TIME_CURRENT_KEY);
 if (getLocalTime) {
   player.setCurrentTime(Number(getLocalTime));
 }
+
+player.on('play');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,64 @@
+// Підключення Ліби
+import throttle from 'lodash.throttle';
 
+// Отримуємо доступ до усіх елементів
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name="email"]'),
+  message: document.querySelector('[name="message"]'),
+};
+
+// Об'єкт сховища
+const dataForm = {};
+
+// Ключ для локального сховища
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+
+// Вішання слухачів
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+// Функція:
+// - заборони відправки форми
+// - очищення форми
+// - очищення локального сховища
+// - виводить в консоль поточні значення
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM_KEY);
+
+  console.log(dataForm);
+}
+
+// Функція запису у локальне сховище + Перевірка
+function onTextareaInput(e) {
+  dataForm[e.target.name] = e.target.value;
+
+  const saveData = () => {
+    try {
+      localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(dataForm));
+    } catch (error) {
+      console.log('error:', error.message);
+    }
+  };
+
+  saveData();
+}
+
+// Функція отримання запису з локального сховища + Перевірка
+function loadData() {
+  try {
+    const getForm = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+    if (getForm) {
+      refs.email.value = getForm.email;
+      refs.message.value = getForm.message;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  return;
+}
+
+loadData();
